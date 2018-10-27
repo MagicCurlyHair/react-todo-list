a35e44bc6b618efdba5a9d3920f430d381c9762c
Heorhi Shakanau <g.shokanov@gmail.com>
2018-10-27 13:51:39 +0300

    Updates

--- precache-manifest.7521c5af5b51524454eacd7a17c5576d.js
+++ precache-manifest.7521c5af5b51524454eacd7a17c5576d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
   },
   {
-    "revision": "1dde5490bcffca92566a",
-    "url": "/react-todo-list/static/js/main.1dde5490.chunk.js"
+    "revision": "938b9ce05cf916f27fbb",
+    "url": "/react-todo-list/static/js/main.938b9ce0.chunk.js"
   },
   {
     "revision": "3fbfc3f2ea8dc7df7935",
     "url": "/react-todo-list/static/js/1.3fbfc3f2.chunk.js"
   },
   {
-    "revision": "1dde5490bcffca92566a",
-    "url": "/react-todo-list/static/css/main.ebc6b7fc.chunk.css"
+    "revision": "938b9ce05cf916f27fbb",
+    "url": "/react-todo-list/static/css/main.a8328cf5.chunk.css"
   },
   {
-    "revision": "003b25a7aa35179ff20cdd1b74a0b75a",
+    "revision": "a1b85192c1faf51f9a594eb09cfcca7a",
     "url": "/react-todo-list/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-todo-list/precache-manifest.d671f203c14258c6832921ea4e6f117b.js"
+  "/react-todo-list/precache-manifest.7521c5af5b51524454eacd7a17c5576d.js"
 );
 
 workbox.clientsClaim();
